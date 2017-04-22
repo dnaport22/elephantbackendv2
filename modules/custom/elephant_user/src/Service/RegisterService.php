14f4361f3d6f5d84604eab141b970ba40f713136
deep-nav <navdeep.dhuti@cameronandwilding.com>
2017-04-22 19:25:02 +0100

    Sending user welcome email upon register

--- modules/custom/elephant_user/src/Service/RegisterService.php
+++ modules/custom/elephant_user/src/Service/RegisterService.php
@@ -30,7 +30,7 @@ class RegisterService {
           RegisterService::htmlEscape($data['pass'])
         );
         $user->save();
-        _user_mail_notify('register_pending_approval', $user);
+        _user_mail_notify('register_no_approval_required', $user);
         // TODO: Send verification email after saving details (try on cloud server).
         return $this->responder->onRegisterSuccessResponse();
       }
