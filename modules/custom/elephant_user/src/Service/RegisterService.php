69b647d68ae8a9c21c655445c43fb09cd6b28b13
deep-nav <navdeep.dhuti@cameronandwilding.com>
2017-04-25 12:02:34 +0100

    Upating mail config

--- modules/custom/elephant_user/src/Service/RegisterService.php
+++ modules/custom/elephant_user/src/Service/RegisterService.php
@@ -32,11 +32,10 @@ class RegisterService {
           RegisterService::htmlEscape($data['pass'])
         );
         $user->save();
+        $this->setVerificationCode($data['email']);
+        _user_mail_notify('register_no_approval_required', $user);
 
-        if ($this->setVerificationCode($data['email'])) {
-          _user_mail_notify('register_no_approval_required', $user);
-          return $this->responder->onRegisterSuccessResponse();
-        }
+        return $this->responder->onRegisterSuccessResponse();
 
       }
       return $this->responder->onNameExistResponse();
