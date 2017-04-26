7f5683bcb4d986ae1b141b49646269fee7305f90
deep-nav <navdeep.dhuti@cameronandwilding.com>
2017-04-26 12:15:47 +0100

    Callback for token replacement

--- modules/custom/elephant_user/src/Service/RegisterService.php
+++ modules/custom/elephant_user/src/Service/RegisterService.php
@@ -33,7 +33,7 @@ class RegisterService {
         );
         $user->save();
         $this->setVerificationCode($data['email']);
-
+        getAccountVerificationUrl();
         _user_mail_notify('register_no_approval_required', $user);
 
         return $this->responder->onRegisterSuccessResponse();
